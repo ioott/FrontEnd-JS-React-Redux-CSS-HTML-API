41e04a92b10dc1eb116a3eedec658b2117dc921d
ioott <vioott@gmail.com>
2022-05-31 19:43:48 -0300

    botando no ar

---
+++ build/precache-manifest.074debca5fab344d676c49634ab8c740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e01663884153aa200c01aa5d6d34d19",
+    "url": "/my-wallet/index.html"
+  },
+  {
+    "revision": "407f234d47c7874658aa",
+    "url": "/my-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c5b4c4bf7688d94bdae",
+    "url": "/my-wallet/static/js/2.34638925.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/my-wallet/static/js/2.34638925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407f234d47c7874658aa",
+    "url": "/my-wallet/static/js/main.bdab0b00.chunk.js"
+  },
+  {
+    "revision": "3cb645c6c6fbc858aa16",
+    "url": "/my-wallet/static/js/runtime-main.35883422.js"
+  }
+]);
